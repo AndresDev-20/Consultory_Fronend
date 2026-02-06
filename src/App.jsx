898a7fdd516c56af40bc30db86ebcf598e66a01f
AndresDev20 <andresmarroquin887@gmail.com>
2026-02-06 15:11:08 -0500

    Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/login/login'
 
 function App() {
 
   return (
     <>
-     <h1>App</h1>
+     <Routes>
+      <Route path='/' element={<Login/>} />
+     </Routes>
     </>
   )
 }
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import "./login.css"
+
+function Login(){
+
+    return (
+        <div className="Login">
+            
+        </div>
+    )
+}
+
+export default Login;
