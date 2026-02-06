0b8c1678c92ec88dd8921b5e246b917b314a12ce
AndresDev20 <andresmarroquin887@gmail.com>
2026-02-06 15:40:27 -0500

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,29 @@
+import { useForm } from "react-hook-form";
 import "./login.css"
 
 function Login(){
+    const { register, handleSubmit, reset } = useForm();
+    const submit = (data) => {
+        console.log(data)
+    }
 
     return (
         <div className="Login">
             <h1 className="Login__title">Bienvenido a Nuffy</h1>
             <div className="Login__container">
                 <h1 className="Login__container-title">Inicia sesión</h1>
-                <form className="Login__container-form">
+                <form onSubmit={handleSubmit(submit)} className="Login__container-form">
                     <div className="Login__form-fields">
                         <label htmlFor="ID">No.identificación:</label>
-                        <input type="number" id="ID" required placeholder="Igresa tu identificación"/>
+                        <input {...register("cc")} type="number" id="ID" required placeholder="Igresa tu identificación"/>
                     </div>
                     <div className="Login__form-fields">
                         <label htmlFor="PASSWORD">Contraseña:</label>
-                        <input type="number" id="PASSWORD" required placeholder="Ingresa tu clave"/>
+                        <input {...register("password")} type="number" id="PASSWORD" required placeholder="Ingresa tu clave"/>
                     </div>
                     <button>Ingresar</button>
                 </form>
-                <div>
+                <div className="Login__recover">
                     <h2>Olvidate la contraseña</h2>
                 </div>
             </div>
