a12e64461d10d29f502d045a6a9bd9fc04ce81aa
AndresDev20 <andresmarroquin887@gmail.com>
2026-08-14 12:25:33 -0500

    Update

---
+++ src/features/patients/PatientId.jsx
@@ -0,0 +1,12 @@
+import "./style/patientId.css";
+
+const PatientId = () => {
+
+    return (
+        <div className="patientId">
+            <h1>PatientId</h1>
+        </div>
+    )
+}
+
+export default PatientId;
